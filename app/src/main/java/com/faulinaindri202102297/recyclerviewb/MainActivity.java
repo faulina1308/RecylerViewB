56be985c2e839ea2774468250a33ebf0736c83c7
faulinaindri <113485041+faulina1308@users.noreply.github.com>
2023-06-08 10:50:51 +0700

    Initial commit

---
+++ app/src/main/java/com/faulinaindri202102297/recyclerviewb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.faulinaindri202102297.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
